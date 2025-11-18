3637228b9b93412f81a890f315822c4f0adf4eca
rrealmrezarajabi <rrealmrezarajabi@gmail.com>
2025-11-18 11:38:36 +0330

    loading skeleton added to products page

--- src/main.jsx
+++ src/main.jsx
@@ -5,10 +5,10 @@ import App from "./App.jsx";
 import { BrowserRouter } from "react-router-dom";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 
-const queryclinet = new QueryClient();
+const queryclient = new QueryClient();
 
 createRoot(document.getElementById("root")).render(
-  <QueryClientProvider client={queryclinet}>
+  <QueryClientProvider client={queryclient}>
     <BrowserRouter>
       <App />
     </BrowserRouter>
--- src/pages/Products.jsx
+++ src/pages/Products.jsx
@@ -1,4 +1,4 @@
-import { useEffect, useState } from "react";
+import { useState } from "react";
 import axios from "axios";
 import { Link } from "react-router-dom";
 import { useQuery } from "@tanstack/react-query";
@@ -9,17 +9,39 @@ const fetchProducts = async () => {
 };
 
 export default function Products() {
+  const [sortType, setSortType] = useState("");
+  const [search, setSearch] = useState("");
+  const [productCount, setProductCount] = useState(null);
+
   const { data, isLoading, isError } = useQuery({
     queryFn: fetchProducts,
     queryKey: ["products"],
+    onSuccess:(res=>{
+      setProductCount(res.length)
+    })
   });
 
   if (isLoading)
     return (
-      <div className="text-center text-blue-400 text-2xl mt-20">
-        Loading products...
+      <div className="p-8 text-center">
+        <h1 className="text-3xl font-bold text-blue-400 mb-6">Products</h1>
+
+        <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-8 max-w-6xl mx-auto">
+          {Array.from({ length: productCount }).map((_, i) => (
+            <div
+              key={i}
+              className="bg-gray-800 p-6 rounded-xl shadow-lg animate-pulse"
+            >
+              <div className="bg-gray-700 h-48 rounded mb-4"></div>
+              <div className="bg-gray-700 h-4 w-3/4 rounded mb-2"></div>
+              <div className="bg-gray-700 h-4 w-1/2 rounded mb-4"></div>
+              <div className="bg-gray-700 h-10 rounded-lg"></div>
+            </div>
+          ))}
+        </div>
       </div>
     );
+
 
   if (isError)
     return (
@@ -28,15 +50,56 @@ export default function Products() {
       </div>
     );
 
+  let sortedProducts = [...data];
+
+  if (sortType === "price-asc") {
+    sortedProducts.sort((a, b) => a.price - b.price);
+  }
+
+  if (sortType === "price-desc") {
+    sortedProducts.sort((a, b) => b.price - a.price);
+  }
+
+  if (sortType === "title-asc") {
+    sortedProducts.sort((a, b) => a.title.localeCompare(b.title));
+  }
+
+  if (sortType === "title-desc") {
+    sortedProducts.sort((a, b) => b.title.localeCompare(a.title));
+  }
+  let filteredProducts = sortedProducts.filter((p) =>
+    p.title.toLowerCase().includes(search.toLowerCase())
+  );
+
   return (
     <div className="p-8 text-center">
       <h1 className="text-3xl font-bold text-blue-400 mb-6">Products</h1>
+      <div className="flex justify-center gap-2 mb-7">
+        <input
+          type="text"
+          value={search}
+          onChange={(e) => setSearch(e.target.value)}
+          placeholder="Search products..."
+          className=" w-[400px] bg-gray-800 text-white p-2  rounded-lg border border-gray-600 focus:ring-2 focus:ring-blue-500 outline-none"
+        />
+        <select
+          value={sortType}
+          onChange={(e) => setSortType(e.target.value)}
+          className="bg-gray-800 text-white p-2 rounded-lg border border-gray-600"
+        >
+          <option value="">Sort By</option>
+          <option value="price-asc">Price: Low to High</option>
+          <option value="price-desc">Price: High to Low</option>
+          <option value="title-asc">Title: A → Z</option>
+          <option value="title-desc">Title: Z → A</option>
+        </select>
+      </div>
 
       <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-8 max-w-6xl mx-auto">
-        {data.map((p) => (
+        {filteredProducts.map((p) => (
           <div
             key={p.id}
-            className="bg-gray-800 p-6 rounded-xl shadow-lg flex flex-col justify-between hover:scale-105 transition-transform"
+            className="bg-gray-800 p-6 rounded-xl shadow-lg hover:scale-105 transition-transform"
           >
             <img
               src={p.image}
